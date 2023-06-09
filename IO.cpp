6c9b5022ec1bcf5cb1b10327ce70f3c156c07857
TeoZerbi <matteo.zerbi@gmail.com>
2023-06-09 11:54:26 +0200

    Modificato errore

--- IO.cpp
+++ IO.cpp
@@ -342,6 +342,11 @@ void IO::outputStep(LB& lb, DEM& dem) {
                 exportFront(lb);
                 break;
             }
+            case SHEAR_CELL_2023:
+			{
+				exportShearCell2023(lb, dem);
+				break;
+			}
 
 
         }
