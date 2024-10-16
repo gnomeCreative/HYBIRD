28075658e5140fdfaa2039855e908648888ecb66
Andrea <andrea.pasqua.ing@gmail.com>
2024-10-16 16:08:47 +0100

    Fixed bug
    Some bugs did not allow CLion to compile (second argument on line 15, 19, and 26 in IO.cpp 'filesystem::create_directories(partDirectory.c_str(), 0777)')

---
+++ cmake-build-debug/CMakeFiles/edit_cache.dir/utility.sh
@@ -0,0 +1,4 @@
+set -e
+
+cd /cygdrive/c/codes/hybird/submarine/HYBIRD/cmake-build-debug
+/cygdrive/c/Users/ci1apa/AppData/Local/JetBrains/CLion2024.2/cygwin_cmake/bin/cmake.exe -E echo No\ interactive\ CMake\ dialog\ available.
