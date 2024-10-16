c496049381e36233c604a0ff94e4e9c2d6593827
Andrea <andrea.pasqua.ing@gmail.com>
2024-10-16 17:34:03 +0100

    Created prototype 5 for immersed cylinder

--- cmake-build-release/CMakeFiles/rebuild_cache.dir/utility.sh
+++
@@ -1,4 +0,0 @@
-set -e
-
-cd /cygdrive/c/codes/hybird/submarine/HYBIRD/cmake-build-release
-/cygdrive/c/Users/ci1apa/AppData/Local/JetBrains/CLion2024.2/cygwin_cmake/bin/cmake.exe --regenerate-during-build -S$(CMAKE_SOURCE_DIR) -B$(CMAKE_BINARY_DIR)
