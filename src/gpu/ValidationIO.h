42163414f71e00e12491c5d773ea685754aca59f
Robert Chisholm <r.chisholm@sheffield.ac.uk>
2024-12-12 14:27:50 +0000

    Add node->f to validation io output

--- src/gpu/ValidationIO.h
+++ src/gpu/ValidationIO.h
@@ -41,7 +41,6 @@ public:
             }
             // Write the header
             fs << "node_count"                      << ",";
-            fs << "coord_average"                   << ",";
             fs << "n_average"                       << ",";
             fs << "uX_average"                      << ",";
             fs << "uY_average"                      << ",";
@@ -60,14 +59,15 @@ public:
             fs << "fluid_count"                     << ",";
             fs << "fluid_coord_average"             << ",";
             fs << "wall_count"                      << ",";
-            fs << "wall_coord_average"              << "\n";
+            fs << "wall_coord_average"              << ",";
+            for (int i = 0; i < lbmDirec; ++i)
+                fs << "f[" <<i <<"]" << ",";
+            fs << "coord_average" << "\n";
         }
         const Node2 nodes = lb.getNodes();
         // Write a new data row
         // node_count
         fs << nodes.count << ",";
-        // coord_average
-        fs << std::accumulate(nodes.coord, nodes.coord + nodes.count, static_cast<uint64_t>(0))/static_cast<float>(nodes.count) << ",";
         // n_average
         fs << std::accumulate(nodes.n, nodes.n + nodes.count, 0.0) / static_cast<float>(nodes.count) << ",";
         // uX_average, uY_average, uZ_average
@@ -115,7 +115,17 @@ public:
         sum = 0;
         for (int i = 0; i < nodes.wallCount; ++i)
             sum += nodes.coord[nodes.wallI[i]];
-        fs << sum / static_cast<float>(nodes.wallCount) << "\n";
+        fs << sum / static_cast<float>(nodes.wallCount) << ",";
+        // f
+        for (int j = 0; j < lbmDirec; ++j) {
+            double sd = 0;
+            for (int i = 0; i < nodes.count; ++i)
+                sd += nodes.f[i * lbmDirec + j];
+            fs << sd / static_cast<float>(nodes.count) << ",";
+        }
+        // coord_average
+        fs << std::accumulate(nodes.coord, nodes.coord + nodes.count, static_cast<uint64_t>(0)) / static_cast<float>(nodes.count) << "\n";
+
     }
     void output(LB& lb) {
         if (!fs.is_open()) {
