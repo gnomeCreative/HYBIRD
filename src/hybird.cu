e22f6fa71613c69d4efeacd78a5febf10ed4096e
Alessandro Leonardi <alessandro.leonardi.ing@gmail.com>
2025-07-08 15:29:22 +0100

    Comment single particle tracking
    Doesn't seem to wrok

--- src/hybird.cu
+++ src/hybird.cu
@@ -235,40 +235,40 @@ void parseConfigFile(IO& io, DEM& dem, LBParams& lb, LBInitParams& lbi, Problem 
     PARSE_CLASS_MEMBER(configFile, io.objectExpTime, "objectExpTime", 0.0);
     PARSE_CLASS_MEMBER(configFile, io.cylinderExpTime, "cylinderExpTime", 0.0);
 
-    // single objects
-    unsigned int totSingleObjects = configFile.vector_variable_size("singleObjects");
-    std::cout << totSingleObjects;
-    if (commandLine.vector_variable_size("-singleObjects") > 0)
-        totSingleObjects = commandLine.vector_variable_size("-singleObjects");
-    cout << "Single objects (" << totSingleObjects << "): ";
-    for (int index = 0; index < totSingleObjects; index++) {
-        int singleObjectHere = 0;
-        PARSE_CLASS_MEMBER_VEC(configFile, singleObjectHere, "singleObjects", index, 0);
-        cout << singleObjectHere << " ";
-        io.singleObjects.push_back(singleObjectHere);
-    }
-    cout << endl;
-    ASSERT(io.singleObjects.size() == totSingleObjects);
-    std::cout << io.singleObjects.size() << std::endl;
-    for (int index = 0; index < totSingleObjects; index++) {
-        std::cout << io.singleObjects[index] << std::endl;
-    }
+    //// single objects
+    //unsigned int totSingleObjects = configFile.vector_variable_size("singleObjects");
+    //std::cout << totSingleObjects;
+    //if (commandLine.vector_variable_size("-singleObjects") > 0)
+    //    totSingleObjects = commandLine.vector_variable_size("-singleObjects");
+    //cout << "Single objects (" << totSingleObjects << "): ";
+    //for (int index = 0; index < totSingleObjects; index++) {
+    //    int singleObjectHere = 0;
+    //    PARSE_CLASS_MEMBER_VEC(configFile, singleObjectHere, "singleObjects", index, 0);
+    //    cout << singleObjectHere << " ";
+    //    io.singleObjects.push_back(singleObjectHere);
+    //}
+    //cout << endl;
+    //ASSERT(io.singleObjects.size() == totSingleObjects);
+    //std::cout << io.singleObjects.size() << std::endl;
+    //for (int index = 0; index < totSingleObjects; index++) {
+    //    std::cout << io.singleObjects[index] << std::endl;
+    //}
 
-    }
-        cout << singleElementHere << " ";
-        io.singleElements.push_back(singleElementHere);
-    //tracking elements
-    unsigned int totSingleElements = configFile.vector_variable_size("singleElements");
-    if (commandLine.vector_variable_size("-singleElements") > 0)
-        totSingleElements = commandLine.vector_variable_size("-singleElements");
-    cout << "Single elements (" << totSingleElements << "): ";
-    for (int index = 0; index < totSingleElements; index++) {
-        int singleElementHere = 0;
-        PARSE_CLASS_MEMBER_VEC(configFile, singleElementHere, "singleElements", index, 0);
-        ASSERT(singleElementHere >= 0);
-    cout << endl;
-    ASSERT(io.singleElements.size() == totSingleElements);
-    std::cout << io.singleElements.size() << std::endl;
+    //}
+    //    cout << singleElementHere << " ";
+    //    io.singleElements.push_back(singleElementHere);
+    ////tracking elements
+    //unsigned int totSingleElements = configFile.vector_variable_size("singleElements");
+    //if (commandLine.vector_variable_size("-singleElements") > 0)
+    //    totSingleElements = commandLine.vector_variable_size("-singleElements");
+    //cout << "Single elements (" << totSingleElements << "): ";
+    //for (int index = 0; index < totSingleElements; index++) {
+    //    int singleElementHere = 0;
+    //    PARSE_CLASS_MEMBER_VEC(configFile, singleElementHere, "singleElements", index, 0);
+    //    ASSERT(singleElementHere >= 0);
+    //cout << endl;
+    //ASSERT(io.singleElements.size() == totSingleElements);
+    //std::cout << io.singleElements.size() << std::endl;
 
 
     // object groups
