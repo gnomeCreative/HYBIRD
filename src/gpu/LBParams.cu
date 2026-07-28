c141ced438a5241bcc45c9019ae6e2a1e526e171
gnomeCreative <alessandro.leonardi.ing@gmail.com>
2026-07-28 17:36:17 +0100

    fixed bug on computing default lambda

--- src/gpu/LBParams.cu
+++ src/gpu/LBParams.cu
@@ -319,9 +319,9 @@ void LBParams::latticeBoltzmannGet(GetPot& configFile, GetPot& commandLine,  LBI
 
         // Project the rectangular domain onto the gravity direction.
         const double gravityLength =
-              fabs(gravityDirection[0]) * lbPhysicalSize[0]
-            + fabs(gravityDirection[1]) * lbPhysicalSize[1]
-            + fabs(gravityDirection[2]) * lbPhysicalSize[2];
+              fabs(gravityDirection.dot(X)) * lbPhysicalSize[0]
+            + fabs(gravityDirection.dot(Y)) * lbPhysicalSize[1]
+            + fabs(gravityDirection.dot(Z)) * lbPhysicalSize[2];
 
         // Ensure that the projected domain length is valid.
         ASSERT(gravityLength > 0.0);
